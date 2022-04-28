d68469aa5ad9cbd4f0e9dd5eeb4ae3f1b5d68be1
Nadelaide <nataliepworden@gmail.com>
2022-04-28 12:48:36 -0700

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,9 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly StoreContext _context; // use underscore for private fields
         public ProductsController(StoreContext context)
@@ -27,9 +25,13 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")] // api/products/ID, returns single product by ID
-        public async Task<ActionResult<Product>> GetProducts(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            return product;
         }
     }
 }
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,19 +1,23 @@
 import { useState, useEffect } from "react";
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 
 export default function Catalog() { //specifies the properties you need
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        //fetch products from API
-        fetch('http://localhost:5000/api/Products')
-            //returns a promise
-            .then(response => response.json()) //turns promise response into json
-            .then(data => setProducts(data)) //setProducts to the data that was returned
+        agent.Catalog.list()
+            .then(products => setProducts(products))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
     }, []) //set empty array as dependency so useEffect is only ever called once instead of every time it's re-rendering
     
+    if (loading) return <LoadingComponent message='Loading products...'/>
+
     return (
         <>
             <ProductList products={products} />
