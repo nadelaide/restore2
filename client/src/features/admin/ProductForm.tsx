6604b1581660a846f83f8b39ddf21b72e6cf3798
Nadelaide <nataliepworden@gmail.com>
2022-06-03 11:58:29 -0700

    Inventory complete

--- client/src/features/admin/ProductForm.tsx
+++ client/src/features/admin/ProductForm.tsx
@@ -1,8 +1,17 @@
 import { Typography, Grid, Paper, Box, Button } from "@mui/material";
 import { useEffect } from "react";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
+import AppDropzone from "../../app/components/AppDropzone";
+import AppSelectList from "../../app/components/AppSelectList";
 import AppTextInput from "../../app/components/AppTextInput";
+import useProducts from "../../app/hooks/useProducts";
 import { Product } from "../../app/models/product";
+import {yupResolver } from '@hookform/resolvers/yup';
+import agent from "../../app/api/agent";
+import { useAppDispatch } from "../../app/store/configureStore";
+import { setProduct } from "../catalog/catalogSlice";
+import { LoadingButton } from "@mui/lab";
+import { validationSchema } from "./ProductValidation";
 
 interface Props {
     product?: Product;
@@ -10,44 +19,79 @@ interface Props {
 }
 
 export default function ProductForm({product, cancelEdit}: Props) {
-    const { control, reset } = useForm();
+    const { control, reset, handleSubmit, watch, formState: {isDirty, isSubmitting} } = useForm({
+        resolver: yupResolver(validationSchema)
+    });
+    const {brands, types } = useProducts();
+    const watchFile = watch('file', null);
+    const dispatch = useAppDispatch();
 
     useEffect(() => {
-        if (product) reset(product);
-    }, [product, reset])
+        if (product && !watchFile && !isDirty) reset(product);
+        return () => {
+            //happens when component is destroyed
+            if (watchFile) URL.revokeObjectURL(watchFile.preview);
+        }
+    }, [product, reset, watchFile, isDirty])
+
+    async function handleSubmitData(data: FieldValues) {
+        try {
+            let response: Product;
+            if (product) {
+                response = await agent.Admin.updateProduct(data);
+            } else {
+                response = await agent.Admin.createProduct(data);
+            }
+            dispatch(setProduct(response));
+            cancelEdit();
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <Box component={Paper} sx={{p: 4}}>
             <Typography variant="h4" gutterBottom sx={{mb: 4}}>
                 Product Details
             </Typography>
+            <form onSubmit={handleSubmit(handleSubmitData)}>
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={12}>
                     <AppTextInput control={control} name='name' label='Product name' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='brand' label='Brand' />
+                    <AppSelectList control={control} items={brands} name='brand' label='Brand' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='type' label='Type' />
+                    <AppSelectList control={control} items={types} name='type' label='Type' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='price' label='Price' />
+                    <AppTextInput type='number' control={control} name='price' label='Price' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='quantityInStock' label='Quantity in Stock' />
+                    <AppTextInput type='number' control={control} name='quantityInStock' label='Quantity in Stock' />
                 </Grid>
                 <Grid item xs={12}>
-                    <AppTextInput control={control} name='description' label='Description' />
+                    <AppTextInput multiline={true} rows={4} control={control} name='description' label='Description' />
                 </Grid>
                 <Grid item xs={12}>
-                    <AppTextInput control={control} name='pictureUrl' label='Image' />
+                    <Box display='flex' justifyContent='space-between' alignItems='center' >
+                    <AppDropzone control={control} name='file' />
+                    {watchFile ? (
+                        <img src={watchFile.preview} alt="preview" style={{maxHeight: 200}} />
+                    ) : (
+                        <img src={product?.pictureUrl} alt={product?.name} style={{maxHeight: 200}} />
+                    )}
+                    </Box>
+
                 </Grid>
             </Grid>
             <Box display='flex' justifyContent='space-between' sx={{mt: 3}}>
                 <Button onClick={cancelEdit} variant='contained' color='inherit'>Cancel</Button>
-                <Button variant='contained' color='success'>Submit</Button>
+                <LoadingButton loading={isSubmitting} variant='contained' color='success'>Submit</LoadingButton>
             </Box>
+                           
+            </form>
         </Box>
     )
 }
