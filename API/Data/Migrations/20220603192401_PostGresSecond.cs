9407970093f5b1172e0548fe3599c251eced3b78
Nadelaide <nataliepworden@gmail.com>
2022-06-03 12:26:26 -0700

    testing

---
+++ API/Data/Migrations/20220603192401_PostGresSecond.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class PostGresSecond : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "9521dbea-c341-481b-a86e-1ddb4faa2bb1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "41c18ca0-457d-4955-a9bc-97c15acc3978");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "74886feb-8faf-4db7-916c-f13b093e3974");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "8b7850cf-d5ff-4573-a86f-2d04c1e7d792");
+        }
+    }
+}
